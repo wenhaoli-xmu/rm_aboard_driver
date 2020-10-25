116eb60e839733d2ddabd46a43b1f9f71fabd993
RainFromCN <RainFromCN@gmail.com>
2020-10-25 22:13:47 +0800

    Create graph.h

---
+++ GRAPH/graph.h
@@ -0,0 +1,24 @@
+#ifndef __GRAPH_H
+#define __GRAPH_H
+
+
+#include "stm32f4xx_hal.h"
+
+
+#define GRAPH_READY				0
+#define GRAPH_BUSY				1
+#define GRAPH_IDLE				2
+#define GRAPH_DONE				3
+
+
+/* 设置GRAPH使用的UART */
+void GRAPH_Open(UART_HandleTypeDef* huart, float* p, float* i, float* d);
+
+/* 发布一个数据点 */
+void GRAPH_Pub(int data);
+
+/* 放在HAL_UART_RxCpltCallback()中 */
+void GRAPH_RxUpdate(UART_HandleTypeDef* huart);
+
+
+#endif
